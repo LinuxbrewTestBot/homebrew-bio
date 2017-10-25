87b2c8e8d27bf1bebb6ec79f49665ea4158926ae
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-25 22:31:50 +0000

    minimap2: add 2.3 bottle for Linuxbrew.

--- Formula/minimap2.rb
+++ Formula/minimap2.rb
@@ -4,6 +4,11 @@ class Minimap2 < Formula
   url "https://github.com/lh3/minimap2/releases/download/v2.3/minimap2-2.3.tar.bz2"
   sha256 "a7bad55254bc4011d4501c2a5a21fd30443ffd04cbcbe28ba190ae160f7955e6"
   head "https://github.com/lh3/minimap2.git"
+  bottle do
+    sha256 "a53d9a577ed3592cd45cb1d3456b27e990c3c64faa163dfc25b1afa99086d346" => :sierra
+    sha256 "ee7edc40aa7ea0f8f5c88b2176bcdfe56dca6cb71581b7c94592d2f19605b90d" => :x86_64_linux
+  end
+
   # doi "https://arxiv.org/abs/1708.01492"
 
   depends_on "zlib" unless OS.mac?
