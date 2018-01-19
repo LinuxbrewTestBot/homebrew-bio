5267afe1f83952be32e61c96b908c33ec1821745
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-19 22:53:51 +0000

    oma: add 2.2.0 bottle for Linuxbrew.

--- Formula/oma.rb
+++ Formula/oma.rb
@@ -3,6 +3,14 @@ class Oma < Formula
   homepage "https://omabrowser.org/standalone/"
   url "https://omabrowser.org/standalone/OMA.2.2.0.tgz"
   sha256 "25ee17b92ef6631507311a93036c91154a42bcee8d033e3a3912836f2764040d"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "4893b28658e53492c7ec7641eaeabcba66f2e3d4edaed4a4e2bc78d4fae0440e" => :sierra
+    sha256 "c0e059b258cd1ac426b9ecb12d15f517e211cd5cb5fd1c696cbf672553fb2cad" => :x86_64_linux
+  end
+
   # cite "http://doi.org/10.1093/nar/gkx1019"
   # cite "http://doi.org/10.1093/bioinformatics/btx229"
   # cite "http://doi.org/10.1093/nar/gku1158"
