24b4932e1e68862ce415565c8ca774fe04030ef5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-06 23:16:21 +0000

    miniasm: add 0.2 bottle for Linuxbrew.

--- Formula/miniasm.rb
+++ Formula/miniasm.rb
@@ -4,6 +4,11 @@ class Miniasm < Formula
   url "https://github.com/lh3/miniasm/archive/v0.2.tar.gz"
   sha256 "177cbb93dbdd3da73e3137296f7822ede830af10339aa7f84fc76afab95a1be6"
   head "https://github.com/lh3/miniasm.git"
+  bottle do
+    sha256 "dbc894c4fbc31b8694777654b4df0e1ce74aff4ef20699c5541a7382f697f475" => :sierra
+    sha256 "db65b66443d72a14d80e228874b9d906e536c52915b7a8d0670b6e9ee96df90d" => :x86_64_linux
+  end
+
   # cite "https://doi.org/10.1093/bioinformatics/btw152"
 
   depends_on "zlib" unless OS.mac?
