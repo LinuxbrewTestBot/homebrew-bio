61c0b2eb8a2490787e752394a6c0024e934edd31
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-07 23:25:27 +0000

    mummer: update 3.23_3 bottle for Linuxbrew.

--- Formula/mummer.rb
+++ Formula/mummer.rb
@@ -7,10 +7,11 @@ class Mummer < Formula
   # cite "https://doi.org/10.1186/gb-2004-5-2-r12"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
     cellar :any_skip_relocation
-    sha256 "08eb9012c2e9466b8945c9bdd6d4397d5ad70b025c102f5a2de1dcbff5175755" => :sierra_or_later
-    sha256 "14086ae0f8420cc5d322f5ccfe614d418a2eb4bdd5304126881a66cc7b33f35b" => :x86_64_linux
+    sha256 "2642a41271e0a4d0522c3d1890d1dfc6ced4ad03aa90ba31c37210f0a96a2e41" => :sierra
+    sha256 "22df96cb1a3e421e5af2e432e3cfc10834ee13fa1e2916aae6a820dbcddcf8c5" => :x86_64_linux
   end
 
   depends_on "tcsh" unless OS.mac?
