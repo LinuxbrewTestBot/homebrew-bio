81244065df18582074f9e40e01e84cf599e25e2b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-25 19:20:34 +0000

    stringtie: add 1.3.4c bottle.

--- Formula/stringtie.rb
+++ Formula/stringtie.rb
@@ -5,6 +5,11 @@ class Stringtie < Formula
   sha256 "5f3595a241349fdb4ff12391ae39d3d86ede31a32244ce9410af36dc42bd5b92"
   head "https://github.com/gpertea/stringtie.git"
 
+  bottle do
+    sha256 "3961cac0570c7d83e9fbe4ea69dbb8ede22973bb4aa1fc016d91bc5f048c8848" => :sierra
+    sha256 "feba49badea9c8672d70e6ac9452f8831fe0b3f4254d6d185de3bb1830792eeb" => :x86_64_linux
+  end
+
   # cite Pertea_2015: "https://doi.org/10.1038/nbt.3122"
 
   depends_on "zlib" unless OS.mac?
