bff63bbdf623d4853e479153eba383bc49e828e7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-01 02:10:40 +0000

    skesa: update 2.1 bottle.

--- Formula/skesa.rb
+++ Formula/skesa.rb
@@ -6,9 +6,8 @@ class Skesa < Formula
   sha256 "2c36e0bdc6372795876fe1ce87cb3a32e66cb113fd5be2b0e2e33f1ded0db138"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "59fb2540d2b5c6a3a6e354ea349eb03054797179aa2a9c2447ed7bd282ac4ab7" => :x86_64_linux
+    sha256 "9ecfd501164b0e6f4f33b0b2363134846c9d4b484e56fe985ad249db8716e3b8" => :x86_64_linux
   end
 
   depends_on :linux
