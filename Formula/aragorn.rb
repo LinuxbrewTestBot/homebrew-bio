6586ce5c44a6e63b4b61ed004fb936f4d95a5467
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-02 09:42:31 +0000

    aragorn: add 1.2.38 bottle.

--- Formula/aragorn.rb
+++ Formula/aragorn.rb
@@ -5,6 +5,14 @@ class Aragorn < Formula
   url "http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/aragorn1.2.38.tgz"
   sha256 "4b84e3397755fb22cc931c0e7b9d50eaba2a680df854d7a35db46a13cecb2126"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "f896ddcae611fbc70e626039eefb08613ccc734ad5bd2c17cd8f514f1531021a" => :sierra
+    sha256 "7670f2b0c66f5567844300f3c7b909cc679948e2fea74657631b025b24ce0520" => :x86_64_linux
+  end
+
   def install
     mv "aragorn#{version}.c", "aragorn.c"
     system "make", "aragorn"
