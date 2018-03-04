8b853e43bb2714d52939941233bfa94b8290f324
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-04 00:45:45 +0000

    trimal: add 1.4.1 bottle.

--- Formula/trimal.rb
+++ Formula/trimal.rb
@@ -6,6 +6,14 @@ class Trimal < Formula
   sha256 "cb8110ca24433f85c33797b930fa10fe833fa677825103d6e7f81dd7551b9b4e"
   head "https://github.com/scapella/trimal"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "e1796872ec4389f87131dea98c4596aa387090d4eb476f1e1472e3763bbc09ed" => :sierra
+    sha256 "e10f7b254386950ff2944caf0b1eb924d0854618485c7cb534ba5f058dc56b17" => :x86_64_linux
+  end
+
   def install
     system "make", "-C", "source", "CC=c++"
     bin.install "source/readal", "source/trimal", "source/statal"
