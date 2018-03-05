abb38273c9c0ea86314a48c1e46a132f3f9e9ce1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-05 07:54:48 +0000

    magic-blast: add 1.3.0 bottle.

--- Formula/magic-blast.rb
+++ Formula/magic-blast.rb
@@ -1,6 +1,14 @@
 class MagicBlast < Formula
   desc "Magic BLAST read mapper"
   homepage "https://ncbi.github.io/magicblast/"
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "234d29db8f7b47403f0cc4ba4823824ed60d72945365dc2bb5de3eb49cb24114" => :sierra
+    sha256 "e05ba857997f6f75a69d5f423202ae9d99b48a122b3c219c6021f99badbbf07b" => :x86_64_linux
+  end
+
   if OS.mac?
     url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/1.3.0/ncbi-magicblast-1.3.0-x64-macosx.tar.gz"
     sha256 "4ed5f93435894e3e121507d0e403b3065171667fa72d2ab39ee550d07265b880"
