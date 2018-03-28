dd1af59996eb308c3822cdf5fcb7b6145169e98d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-28 03:29:52 +0000

    uniqtag: add 1.0 bottle.

--- Formula/uniqtag.rb
+++ Formula/uniqtag.rb
@@ -6,6 +6,14 @@ class Uniqtag < Formula
   sha256 "8ff0dd850c15ff3468707ae38a171deb6518866a699964a1aeeec9c90ded7313"
   head "https://github.com/sjackman/uniqtag.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "7579221b25d406cab550687a6d083683f2f4d1060372c5901aec58ca20adf1fc" => :sierra
+    sha256 "11701749a7b1189fb21658a8f959be0942027ff91162742fb11cdabf3b6a1b5b" => :x86_64_linux
+  end
+
   depends_on "ruby" unless OS.mac?
 
   def install
