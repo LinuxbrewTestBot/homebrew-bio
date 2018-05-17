8be128baffb111f3a45db87b6c13317ead69bf61
suujia <suujia@icloud.com>
2018-05-17 13:40:59 -0700

    installed LWP::Simple

--- Formula/simulate-pcr.rb
+++ Formula/simulate-pcr.rb
@@ -2,6 +2,7 @@ class SimulatePcr < Formula
   # cite Gardner_2014: "https://doi.org/10.1186/1471-2105-15-237"
   desc "Predicts amplicon products from single or multiplex primers"
   homepage "https://sourceforge.net/projects/simulatepcr/"
+  
   url "https://downloads.sourceforge.net/project/simulatepcr/simulate_PCR-v1.2.tar.gz"
   sha256 "022d1cc595d78a03b6a8a982865650f99d9fa067997bfea574c2416cc462e982"
 
@@ -10,8 +11,12 @@ class SimulatePcr < Formula
   depends_on "LWP::Simple" => :perl
 
   def install
+    libexec.install Dir["*"]
     bin.install "simulate_PCR"
     inreplace bin/"simulate_PCR", "#!/usr/bin/perl", "#!/usr/bin/env perl"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    system "cpanm", "--self-contained", "-l", libexec, "LWP::Simple"
+    (bin/"simulate-pcr").write_env_script("#{libexec}/bin/simulate-pcr", :PERL5LIB => ENV["PERL5LIB"])
   end
 
   test do
