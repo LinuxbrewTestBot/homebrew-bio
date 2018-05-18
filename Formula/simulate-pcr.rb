1dc9cbed3bd68e5e6445a65cf8c1a4685ccf8ede
suujia <suujia@icloud.com>
2018-05-18 13:00:20 -0700

    syntax

--- Formula/simulate-pcr.rb
+++ Formula/simulate-pcr.rb
@@ -2,7 +2,7 @@ class SimulatePcr < Formula
   # cite Gardner_2014: "https://doi.org/10.1186/1471-2105-15-237"
   desc "Predicts amplicon products from single or multiplex primers"
   homepage "https://sourceforge.net/projects/simulatepcr/"
-  
+
   url "https://downloads.sourceforge.net/project/simulatepcr/simulate_PCR-v1.2.tar.gz"
   sha256 "022d1cc595d78a03b6a8a982865650f99d9fa067997bfea574c2416cc462e982"
 
