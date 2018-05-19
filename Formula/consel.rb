6169858f58093b24f04d00b756fc18d8c316999d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-19 04:50:09 +0000

    consel: add 0.20 bottle.

--- Formula/consel.rb
+++ Formula/consel.rb
@@ -4,6 +4,14 @@ class Consel < Formula
   url "http://stat.sys.i.kyoto-u.ac.jp/prog/consel/pub/cnsls020.tgz"
   version "0.20"
   sha256 "cc6e8ee6077693817db475229aa47b9d9fb66bf5f48a3aace63e4b525f05238b"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "a27d35514c4c834befc6bf71f9beb91a09369190c73830c34f6fecba817857be" => :sierra_or_later
+    sha256 "d8a7fd3a42022107b4fd61b490be3cfbc2248ccae15fd580853ab705ad9b6e2b" => :x86_64_linux
+  end
+
   # cite Shimodaira_2001: "https://doi.org/10.1093/bioinformatics/17.12.1246"
 
   def install
