cb18f65ee1c054a9e54c9350872990e4bafe1465
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-22 14:20:13 +0000

    sortmerna: add 2.1 bottle.

--- Formula/sortmerna.rb
+++ Formula/sortmerna.rb
@@ -1,6 +1,13 @@
 class Sortmerna < Formula
   desc "SortMeRNA: filter metatranscriptomic ribosomal RNA"
   homepage "http://bioinfo.lifl.fr/RNA/sortmerna/"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "aa26fd0b1316228b17fcb700bab6a2121af7806435c1657c9cbbf8724ce1f47d" => :sierra_or_later
+    sha256 "b412dde11f5cb06f8c4a1aa3d78719d0647ffe5a2e9ff81df960903346269bd1" => :x86_64_linux
+  end
+
   # doi "10.1093/bioinformatics/bts611"
   # tag "bioinformatics"
 
