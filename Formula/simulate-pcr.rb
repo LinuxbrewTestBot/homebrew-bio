e6c4962a36b7d2337df437a6ebf972e7e52357cc
Susie C <suujia@icloud.com>
2018-06-21 16:01:04 -0700

    Update simulate-pcr.rb

--- Formula/simulate-pcr.rb
+++ Formula/simulate-pcr.rb
@@ -14,9 +14,9 @@ class SimulatePcr < Formula
   def install
     bin.install "simulate_PCR"
     inreplace bin/"simulate_PCR", "#!/usr/bin/perl", "#!/usr/bin/env perl"
+    ENV.prepend "PERL5LIB", Formula["bioperl"].libexec/"lib/perl5"
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
     system "cpanm", "--self-contained", "-l", libexec, "IO::Socket::SSL", "LWP"
-    system "cpanm", "--self-contained", "-l", libexec, "--force", "Bio::SeqIO"
     bin.env_script_all_files libexec, "PERL5LIB" => ENV["PERL5LIB"]
   end
 
