e92b9b3a9594d93e8ffbf3bd16c200acd5bcbcbc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-21 23:18:04 +0000

    simulate-pcr: add 1.2 bottle.

--- Formula/simulate-pcr.rb
+++ Formula/simulate-pcr.rb
@@ -6,6 +6,13 @@ class SimulatePcr < Formula
   url "https://downloads.sourceforge.net/project/simulatepcr/simulate_PCR-v1.2.tar.gz"
   sha256 "022d1cc595d78a03b6a8a982865650f99d9fa067997bfea574c2416cc462e982"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "2bfe33208a09f150e4a6474b7bd46b36d18ad720da14a7bf95c649ab7b2138f8" => :sierra_or_later
+    sha256 "41c781720102d0d12df2ce4e22122ec2a8b58be8c7df9dda0699243bed224eaa" => :x86_64_linux
+  end
+
   depends_on "cpanminus" => :build
   depends_on "bioperl"
   depends_on "blast"
