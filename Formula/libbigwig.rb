c8ffc74d57678fc3ef31f4325cbeff19271b675d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-15 21:20:44 +0000

    libbigwig: update 0.4.4 bottle.

--- Formula/libbigwig.rb
+++ Formula/libbigwig.rb
@@ -5,10 +5,9 @@ class Libbigwig < Formula
   sha256 "43a2298b2ebadc48103447a3bb4426df1b38d1bec5fa564e50ed2f00cc060478"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any
-    sha256 "6c8853bfae2b374bfb6949036cb64b39667dfb5a5b1be9c92bcb61df867931b2" => :sierra
-    sha256 "46011c89b465d546b60c616d7510be4c615b2836e3781000f7c721b95cb48ed9" => :x86_64_linux
+    sha256 "3513fd0a41b0bf1409cb8f1e5ce9113811ece9d48c34a1a8cb2d734122d1c531" => :sierra
+    sha256 "f85dc5dd0b892d1bcaeb4bec2623085f3a15e73adc115aa2fac119889d42131e" => :x86_64_linux
   end
 
   unless OS.mac?
