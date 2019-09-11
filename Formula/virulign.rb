f30f21200c36ffbf246b54f9e7b48dec849c49dc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-11 23:44:17 +0000

    virulign: update 1.0.2 bottle.

--- Formula/virulign.rb
+++ Formula/virulign.rb
@@ -7,8 +7,8 @@ class Virulign < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c0d4f449f8c8777390a014d11f90900cde48d1932af55457ca83c01ddd8692a8" => :sierra
-    sha256 "c3a6c7aeddd98e5b8346534a1650e46ed4c5f72416f82cda23cc07e16358120a" => :x86_64_linux
+    sha256 "aa82baa295f493e1627a4f9a4efc724c966ae4dc52226b65ba4cfda4fd7b9806" => :sierra
+    sha256 "88d9376e8e85dc9ee5b8328753f3205814ed2721ba07779202fecbb86e2e09e8" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
