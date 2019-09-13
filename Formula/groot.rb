37d47114e7566b29e6b9bfbebeb8b7fad6305b81
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-13 00:06:43 +0000

    groot: add 0.8.5 bottle.

--- Formula/groot.rb
+++ Formula/groot.rb
@@ -2,6 +2,12 @@ class Groot < Formula
   # cite Rowe_2018: "https://doi.org/10.1093/bioinformatics/bty387"
   desc "Resistome profiler for Graphing Resistance Out Of meTagenomes"
   homepage "https://github.com/will-rowe/groot"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d1594f5953a4578c4c9d842d8bf115a31da9702a30809969e62fcb715e4587f2" => :sierra
+    sha256 "d29ed2ac8a75a997bd4fad81c578af10a94dc8a20f68ab98c3c779ed9f5ed2bc" => :x86_64_linux
+  end
+
   if OS.mac?
     url "https://github.com/will-rowe/groot/releases/download/0.8.5/groot_osx.gz"
     sha256 "484417279ea50117496f095a3f42febe868cd7f4391c5e6c91e393c72f1e1945"
