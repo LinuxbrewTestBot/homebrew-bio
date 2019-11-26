4208838e0694a0ea045d0986123582a0729e1e79
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-26 21:11:44 +0000

    fast-gep: add 1.0 bottle.

--- Formula/fast-gep.rb
+++ Formula/fast-gep.rb
@@ -5,6 +5,12 @@ class FastGep < Formula
   url "https://github.com/jizhang-nz/fast-GeP/archive/1.0.tar.gz"
   sha256 "017ac4b34aa026b5a97460a9846c01d53eaed2b5ce4da792df8535a6e58ab7d8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "57ae16ee53f1e5f96038f3ed70cd710c84f845843d5c3a7d650425d8cbd48a2b" => :mojave
+    sha256 "6e225923fb29cfd58f6287d0d9cacfe81fbab6145724704a873c58a1dcbaaa4b" => :x86_64_linux
+  end
+
   depends_on "blast"
   depends_on "diamond"
   depends_on "mafft"
